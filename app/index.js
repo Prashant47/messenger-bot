253ff22b1b1f3a8fe1b50523bf6f0b93681a56bd
Prashant Gonarkar <prashantgonarkar@gmail.com>
2016-05-03 14:03:54 +0530

    added POST method on webook

--- app/index.js
+++ app/index.js
@@ -23,4 +23,45 @@ app.get('/webhook', function (req, res) {
   }
 });
 
+// @POST /webhook
+app.post('/webhook/', function (req, res) {
+  messaging_events = req.body.entry[0].messaging;
+  for (i = 0; i < messaging_events.length; i++) {
+    event = req.body.entry[0].messaging[i];
+    sender = event.sender.id;
+    if (event.message && event.message.text) {
+      text = event.message.text;
+      // Handle a text message from this sender
+       sendTextMessage(sender, "Text received, echo: "+ text.substring(0, 200));
+    }
+  }
+  res.sendStatus(200);
+});
+
+
+// Page Token
+var token = "EAAclaRZBb5CsBAPXB3g85BZCw7bms8zmh03kFOMxVkuiBVQiLMkXxG2AQKVfRVuEO4ZAKmZBgq8ZAkTJYZAdnGZBwHIALh5AfU0XeWRivZCMMIIrMkF5JxgMJN8ZCjMKGX622snpddZBEI5xkZBUaowDZC0nVNCIMs8jM2kXLZCmWa4DyHgZDZD";
+
+function sendTextMessage(sender, text) {
+  messageData = {
+    text:text
+  }
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token:token},
+    method: 'POST',
+    json: {
+      recipient: {id:sender},
+      message: messageData,
+    }
+  }, function(error, response, body) {
+    if (error) {
+      console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+      console.log('Error: ', response.body.error);
+    }
+  });
+}
+
+
 app.listen();
